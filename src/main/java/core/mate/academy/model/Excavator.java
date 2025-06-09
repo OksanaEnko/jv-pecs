eb424de0b401cad8f817e70d434cacc20c8a3b07
OksanaEnko <132217385+OksanaEnko@users.noreply.github.com>
2025-06-10 00:05:22 +0200

    Update Excavator.java

--- src/main/java/core/mate/academy/model/Excavator.java
+++ src/main/java/core/mate/academy/model/Excavator.java
@@ -1,11 +1,17 @@
 package core.mate.academy.model;
 
-/**
- * Add some custom fields that could be only in Excavator
- * Do not remove no-args constructor
- */
 public class Excavator extends Machine {
+    private int armLength;
+
     public Excavator() {
+    }
+
+    public int getArmLength() {
+        return armLength;
+    }
+
+    public void setArmLength(int armLength) {
+        this.armLength = armLength;
     }
 
     @Override
