c408c04a436ffc8ea5da546f1f95dee8a8b4b3f3
OksanaEnko <132217385+OksanaEnko@users.noreply.github.com>
2025-06-12 12:32:13 +0200

    Update Bulldozer.java

--- src/main/java/core/mate/academy/model/Bulldozer.java
+++ src/main/java/core/mate/academy/model/Bulldozer.java
@@ -1,11 +1,17 @@
 package core.mate.academy.model;
 
-/**
- * Add some custom fields that could be only in Bulldozer
- * Do not remove no-args constructor
- */
 public class Bulldozer extends Machine {
+    private boolean hasBlade;
+
     public Bulldozer() {
+    }
+
+    public boolean isHasBlade() {
+        return hasBlade;
+    }
+
+    public void setHasBlade(boolean hasBlade) {
+        this.hasBlade = hasBlade;
     }
 
     @Override
